277481251adfde4cc50d61e1d7f3ae761104a70d
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2024-01-01 13:52:18 -0300

    feat: persistence model with entity manager

--- src/main/java/com/marcos/loja/Main.java
+++ src/main/java/com/marcos/loja/Main.java
@@ -1,7 +1,25 @@
 package com.marcos.loja;
 
+import com.marcos.loja.modelo.Produto;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+import java.math.BigDecimal;
+
 public class Main {
     public static void main(String[] args) {
+        Produto celular = new Produto();
+        celular.setNome("Xiaomi Redmi");
+        celular.setDescricao("Teste");
+        celular.setPreco(new BigDecimal("1200"));
 
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("jpa");
+        EntityManager em = factory.createEntityManager();
+
+        em.getTransaction().begin();
+        em.persist(celular);
+        em.getTransaction().commit();
+        em.close();
     }
 }
