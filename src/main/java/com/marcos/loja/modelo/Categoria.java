c9894cf0dd46d981c21f190358c9563219efa28d
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2024-01-06 11:22:12 -0300

    feat: composite key

--- src/main/java/com/marcos/loja/modelo/Categoria.java
+++ src/main/java/com/marcos/loja/modelo/Categoria.java
@@ -6,23 +6,29 @@ import jakarta.persistence.*;
 @Table(name = "categorias")
 public class Categoria {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String nome;
+    @EmbeddedId
+    private CategoriaId id;
 
     public Categoria() {
     }
 
     public Categoria(String nome) {
-        this.nome = nome;
+        this.id = new CategoriaId(nome, "XPTO");
     }
 
     public String getNome() {
-        return nome;
+        return this.id.getNome();
     }
 
     public void setNome(String nome) {
-        this.nome = nome;
+        this.id.setNome(nome);
+    }
+
+    public String getTipo() {
+        return this.id.getTipo();
+    }
+
+    public void setTipo(String tipo) {
+        this.id.setTipo(tipo);
     }
 }
